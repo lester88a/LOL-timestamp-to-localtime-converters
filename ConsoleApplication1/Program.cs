d4881e8548561e82fb1b96c9ed2c4b67df97eea0
lester88a <lester88a@gmail.com>
2016-06-15 17:00:19 -0400

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            long timestamp = 1465702102000;
+            string localTime =  ConvertToLocalTime(timestamp);
+            Console.WriteLine(localTime);
+        }
+        
+        public static string ConvertToLocalTime(long timestamp)
+        {
+            var posixTime = DateTime.SpecifyKind(new DateTime(1970, 1, 1), DateTimeKind.Utc);
+            var time = posixTime.AddMilliseconds(timestamp);
+            TimeZone localZone = TimeZone.CurrentTimeZone;
+            var localTime = localZone.ToLocalTime(time);
+
+            return localTime.ToString();
+        }
+    }
+}
